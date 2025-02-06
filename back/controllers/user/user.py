a251361aa432f885449df3d15b9447d967e0dd1d
Vladimir Chumakov <vladimir.973@list.ru>
2025-02-07 01:44:59 +0300

    fix rating

--- back/controllers/user/user.py
+++ back/controllers/user/user.py
@@ -41,7 +41,7 @@ class UserController(BaseUserController):
                     tg_id=tg_id,
                     username=username,
                     is_premium=is_premium,
-                    rating=0.00,
+                    rating=5.00,
                     total_sales=0.0,
                     is_vip=False,
                     last_login=datetime.now(timezone.utc),
