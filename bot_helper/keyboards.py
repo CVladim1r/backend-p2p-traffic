b81da38c9879572de8ff27148e0f53946aa12a62
Vladimir Chumakov <vladimir.973@list.ru>
2025-02-16 22:58:27 +0300

    add bot f

--- bot_helper/keyboards.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-start_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    # [InlineKeyboardButton(text="📋 Частые вопросы", callback_data="faq")],
-    [InlineKeyboardButton(text="🌐 Наш телеграмм канал", url="https://t.me/Justadchanel")]
-])
