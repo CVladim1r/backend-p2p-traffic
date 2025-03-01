f56a88e4bee89386cfc64f3439b6bf9f6af4285e
Vladimir Chumakov <vladimir.973@list.ru>
2025-03-01 19:59:34 +0300

    fix models

--- back/models/prize.py
+++ back/models/prize.py
@@ -1,9 +1,10 @@
 from tortoise.models import Model
 from tortoise import fields
-
+from uuid import uuid4
 from back.models.enums import PrizeType
 
 class ActivePrize(Model):
+    uuid = fields.UUIDField(pk=True, default=uuid4)
     user = fields.ForeignKeyField('models.Users', related_name='prizes')
     prize_type = fields.CharEnumField(enum_type=PrizeType)
     expires_at = fields.DatetimeField()
